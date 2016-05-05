37767b8ed0c3af4df5d3274cb2e07b91c2349fd2
jazzi <jazzihong22@gmail.com>
2016-05-05 22:59:08 +0800

    add content

--- about.php
+++ about.php
@@ -1,18 +1,17 @@
 <section class="about">
     <div class="body">
         <p>
-            <img alt="我的照片"
+            <img alt="logo"
                 src="<?php $this->options->themeUrl('images/avatar.png'); ?>" />
         </p>
-        <p>嗨！此处只有简单好茶，腔调全无，如欲购买，请移步微信公众号<em>「茶行特力」</em>。</p>
-        <p>除了我的博客，同时也欢迎您访问我的 <a href="https://github.com/feelinglucky">GitHub</a>
-            、 <a href="https://twitter.com/feelinglucky">Twitter</a>、<a href="http://instagram.com/feelinglucky">Instagram</a> 主页。</p>
+        <p>嗨！此处只有简单好茶，腔调侬无，如欲购买，请移步微信公众号<em>「茶行特力」</em>。</p>
 
-        <p>这个 Blog 原先的名字叫 <em>Gracecode.com</em> 、现在叫 <em>「無標題文檔」</em> 。
+        <p>Welcome to Chaxing Teli online tea shop, international shipping service is available, pls <a href=mailto:teahacker@qq.com>drop us an email</a> once have any questions.</p>
 
-        其实无所谓叫什么名字，因为我曾经为这个名字伤透了脑筋。最后想到的这个名字都没啥特别的，说到
-底是因为我实在给它不了个非常酷的名字。</p> 
+        <p>一直以来对茶叶的传统门店销售方式无甚好感，那种堆砌历史字画的“高大上”拒绝了好多人走进去的念头；而现在一些所谓的网店则陷入低质低价的恶性循环，让原本喝茶这么一件大开心的事情，因为购买时的疑虑重重而大大扫兴。</p>
 
-        <p>如果您想联系我，可以发我邮件 <kbd>echo bHVja3lAZ3JhY2Vjb2RlLmNvbQo= | base64 --decode</kbd>
+       <p>我们不以“安全”为卖点，因为我们深信安全是<em>理所当然、无需赘言</em>的，我们的目标是创造一种简单的快乐，就是简简单单的一杯好茶。</p>
+
+
     </div>
 </section>
